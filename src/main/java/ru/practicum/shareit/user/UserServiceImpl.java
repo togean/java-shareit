8e9581ad9044c827e77e2ffad665d3901439b746
Ger <togean@yandex.ru>
2025-02-01 18:04:33 +0300

    Домашнее Задание 15. Подправил стили

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -10,7 +10,6 @@ import ru.practicum.shareit.user.dao.UserRepository;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
-
 import java.util.List;
 import java.util.Optional;
 
